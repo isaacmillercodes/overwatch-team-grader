6b62f618aea0ac2867a45c632b7bd702daa10f8c
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-11-10 10:01:15 -0700

    aligned character rows and added overwatch font

--- js/components/heroes/heroes.controller.js
+++ js/components/heroes/heroes.controller.js
@@ -10,31 +10,50 @@
 
   function heroController(HeroService) {
     /*jshint validthis: true */
+    // HeroService.getAll('heroes').then(heroList => {
+    //   const offenseHeroes = [];
+    //   const defenseHeroes = [];
+    //   const tankHeroes = [];
+    //   const supportHeroes = [];
+    //   heroList.data.forEach(hero => {
+    //     switch (hero.role) {
+    //       case 'Offense':
+    //         offenseHeroes.push(hero);
+    //         break;
+    //       case 'Defense':
+    //         defenseHeroes.push(hero);
+    //         break;
+    //       case 'Tank':
+    //         tankHeroes.push(hero);
+    //         break;
+    //       case 'Support':
+    //         supportHeroes.push(hero);
+    //         break;
+    //     }
+    //   });
+    //   this.offenseHeroes = offenseHeroes;
+    //   this.defenseHeroes = defenseHeroes;
+    //   this.tankHeroes = tankHeroes;
+    //   this.supportHeroes = supportHeroes;
+    // });
+
+    const topRowHeroes = [];
+    const middleRowHeroes = [];
+    const bottomRowHeroes = [];
+
     HeroService.getAll('heroes').then(heroList => {
-      const offenseHeroes = [];
-      const defenseHeroes = [];
-      const tankHeroes = [];
-      const supportHeroes = [];
       heroList.data.forEach(hero => {
-        switch (hero.role) {
-          case 'Offense':
-            offenseHeroes.push(hero);
-            break;
-          case 'Defense':
-            defenseHeroes.push(hero);
-            break;
-          case 'Tank':
-            tankHeroes.push(hero);
-            break;
-          case 'Support':
-            supportHeroes.push(hero);
-            break;
+        if (topRowHeroes.length < 8) {
+          topRowHeroes.push(hero);
+        } else if (middleRowHeroes.length < 8) {
+          middleRowHeroes.push(hero);
+        } else {
+          bottomRowHeroes.push(hero);
         }
       });
-      this.offenseHeroes = offenseHeroes;
-      this.defenseHeroes = defenseHeroes;
-      this.tankHeroes = tankHeroes;
-      this.supportHeroes = supportHeroes;
+      this.topHeroes = topRowHeroes;
+      this.middleHeroes = middleRowHeroes;
+      this.bottomHeroes = bottomRowHeroes;
     });
   }
 
