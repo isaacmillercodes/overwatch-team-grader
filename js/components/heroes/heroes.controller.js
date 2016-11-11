0a534d183c99d6e796090e0eacf8840245315ef0
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-11-10 19:17:09 -0700

    grade message now comes from controller

--- js/components/heroes/heroes.controller.js
+++ js/components/heroes/heroes.controller.js
@@ -29,20 +29,23 @@
       this.bottomHeroes = bottomRowHeroes;
     });
 
+    this.gradeMessage = 'Add more heroes to your team!';
+
     this.selectHero = (hero) => {
-      if (this.selectedHeroes.length < 6) {
 
-        let matchingHero = this.selectedHeroes.filter(teamHero => {
-          return hero.id === teamHero.id;
-        })[0];
+      // this.gradeMessage = 'Add ' + (5 - this.selectedHeroes.length) + ' more heroes to your team!';
 
-        if (!matchingHero) {
+      let matchingHero = this.selectedHeroes.filter(teamHero => {
+        return hero.id === teamHero.id;
+      })[0];
+
+      if (!matchingHero) {
+        if (this.selectedHeroes.length < 6) {
           return this.selectedHeroes.push(hero);
-        } else {
-          let index = this.selectedHeroes.indexOf(matchingHero);
-          return this.selectedHeroes.splice(index, 1);
         }
-
+      } else {
+        let index = this.selectedHeroes.indexOf(matchingHero);
+        return this.selectedHeroes.splice(index, 1);
       }
 
     };
