b182206430254e6fba8c27210a87b6fa8e8b8f3f
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-11-10 21:59:33 -0700

    grade message for no support heroes no displaying

--- js/components/heroes/heroes.controller.js
+++ js/components/heroes/heroes.controller.js
@@ -29,8 +29,6 @@
       this.bottomHeroes = bottomRowHeroes;
     });
 
-    this.gradeMessage = 'Add more heroes to your team!';
-
     this.selectHero = (hero) => {
 
       // this.gradeMessage = 'Add ' + (5 - this.selectedHeroes.length) + ' more heroes to your team!';
@@ -48,6 +46,55 @@
         return this.selectedHeroes.splice(index, 1);
       }
 
+    };
+
+    this.gradeTeam = (teamArray) => {
+      const teamGrades = [0, 0, 0, 0];
+      teamArray.forEach(hero => {
+        if (hero.role === 'Offense') {
+          teamGrades[0]++;
+        } else if (hero.role === 'Defense') {
+          teamGrades[1]++;
+        } else if (hero.role === 'Tank') {
+          teamGrades[2]++;
+        } else {
+          if (hero.secondary !== 'Passive Damage') {
+            teamGrades[3]++;
+          } else {
+            teamGrades[1]++;
+          }
+        }
+      });
+
+      // switch (teamGrades) {
+      //   case [0, 2, 2, 2]:
+      //     this.gradeMessage = 'Look at this team! You\'re gonna do great.';
+      //     this.messageColor = '#74ce39';
+      //     break;
+      //   case [2, 0, 2, 2]:
+      //     this.gradeMessage = 'Look at this team! You\'re gonna do great.';
+      //     this.messageColor = '#74ce39';
+      //     break;
+      //   case [1, 1, 2, 2]:
+      //     this.gradeMessage = 'Look at this team! You\'re gonna do great.';
+      //     this.messageColor = '#74ce39';
+      //     break;
+      // }
+
+      console.log(teamGrades);
+
+      if (teamGrades[3] === 0) {
+        this.gradeMessage = 'Heroes never die! Or, at least they don\'t when they have a healer. Try adding a healing support like Mercy or Lúcio!' ;
+        this.messageColor = '#faa02e';
+        console.log('we made it!');
+      }
+
+      this.gradedTeam = !this.gradedTeam;
+
+      // this.gradeMessage = 'Wow, such team!';
+      // this.messageColor = '#74ce39';
+      // this.messageColor = '#b04a33';
+      // this.messageColor = '#faa02e';
     };
 
   }
