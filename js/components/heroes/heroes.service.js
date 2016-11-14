d0ef973135bf6af89a90b187750fdb94fdf4c2fd
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-11-14 15:50:33 -0700

    adding loading screen for bootstrap issue

--- js/components/heroes/heroes.service.js
+++ js/components/heroes/heroes.service.js
@@ -11,7 +11,7 @@
   function HeroService($http) {
     /*jshint validthis: true */
     const baseUrl = 'https://overwatch-hero-api.herokuapp.com/api/v1/';
-    
+
     this.getAll = (resource) => {
       return $http.get(`${baseUrl}${resource}`);
     };
